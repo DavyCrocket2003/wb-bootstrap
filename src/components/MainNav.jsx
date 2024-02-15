d2c3eefab77483f76364eebeea6d340cfa7548b7
David Gann <davycrocket2003@gmail.com>
2024-02-15 15:44:40 -0700

    initial commit

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,25 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+
+export default function MainNav({ brand, leftLinks, rightLinks }) {
+  return (
+    <Navbar expand="lg" className="bg-success navbar-dark">
+      <Container fluid>
+        <Navbar.Brand href="/">{brand}</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className='me-auto'>
+              {leftLinks.map(({ url, text }, idx) => (
+                  <Nav.Link key={idx} href={url}>{text}</Nav.Link>
+              ))}
+          </Nav>
+          <Nav>
+              {rightLinks.map(({ url, text }, idx) => (
+                  <Nav.Link key={idx} href={url}>{text}</Nav.Link>
+              ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
